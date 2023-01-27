ccbfda6fcb48fa1f264484b0092fa3cae81c5d28
Zobayer Hossain <90368480+Zobayer6@users.noreply.github.com>
2023-01-28 01:29:21 +0600

    practice3

---
+++ practice3.js
@@ -0,0 +1,11 @@
+var a = 13;
+var b = 79;
+var c = 45;
+
+if (a > b) {
+  console.log("a bigger then b");
+} else if (a > c) {
+  console.log("a is bigger then c");
+} else if (b > c) {
+  console.log("b is biggest number");
+}
