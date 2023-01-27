30c3fbb608d844524a02adf92b670c989854a6a9
Zobayer Hossain <90368480+Zobayer6@users.noreply.github.com>
2023-01-28 01:16:09 +0600

    practice2 solved

---
+++ practice2.js
@@ -0,0 +1,17 @@
+var meScore = 51;
+var tomScore = 66;
+var jeneScore = 95;
+var peterScore = 56;
+var johnScore = 40;
+
+if (80 <= meScore) {
+  console.log("Grade is A");
+} else if (60 <= meScore) {
+  console.log("Grade is B");
+} else if (50 <= meScore) {
+  console.log("Grade is C");
+} else if (40 <= meScore) {
+  console.log("Grade is D");
+} else if (39 >= meScore) {
+  console.log("Fail");
+}
