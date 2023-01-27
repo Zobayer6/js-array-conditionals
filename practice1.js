0c0231404773b8416fe299a8e1cba3e4702a84bd
Zobayer Hossain <90368480+Zobayer6@users.noreply.github.com>
2023-01-28 01:00:51 +0600

    practice problem 1 solved

---
+++ practice1.js
@@ -0,0 +1,15 @@
+// a)
+
+var fruits = ["Apple", "Banana", "Orange"];
+var position = fruits.indexOf("Banana");
+console.log(position);
+
+// b)
+
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push("Watermelon");
+console.log(fruits);
